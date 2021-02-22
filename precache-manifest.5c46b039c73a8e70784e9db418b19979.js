f725da9fa4fe77134e5ec1ec67d1a26a93ff96a3
indhu-github <indhusworkmail@gmail.com>
2021-02-22 12:42:23 +0530

    Updates

---
+++ precache-manifest.5c46b039c73a8e70784e9db418b19979.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b703e4d42e847cb24baf18afff700659",
+    "url": "/Youtube-clone-Reactjs/index.html"
+  },
+  {
+    "revision": "37b72ce75e82a36d6a8a",
+    "url": "/Youtube-clone-Reactjs/static/js/2.850edfd7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube-clone-Reactjs/static/js/2.850edfd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28a82fb50fb5d10805eb",
+    "url": "/Youtube-clone-Reactjs/static/js/main.b188bcf6.chunk.js"
+  },
+  {
+    "revision": "2cc2ca62d06549613260",
+    "url": "/Youtube-clone-Reactjs/static/js/runtime-main.96eedea9.js"
+  }
+]);
